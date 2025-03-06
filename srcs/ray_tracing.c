f82708c8bf7fb6274ebc021e5c5167c93a4f8da8
masakikaihori <nana7hachi89@gmail.com>
2025-03-06 18:49:27 +0900

    not good

--- srcs/ray_tracing.c
+++ srcs/ray_tracing.c
@@ -13,22 +13,22 @@ void	ray_sphere(t_xyz ray, t_sphere obj, t_hit *hit, t_xyz camera)
 	float	f[4];
 
 	f[A] = pow(vec_norm(ray), 2.0);
-	f[B] = 2 * (innner_product(camera, ray) - innner_product(obj.coord, ray));
+	f[B] = 2.0 * (innner_product(camera, ray) - innner_product(obj.coord, ray));
 	f[C] = pow(vec_norm(vec_subtraction(camera, obj.coord)), 2.0) - pow(obj.diameter, 2.0);
-	f[D] = pow(f[B], 2.0) - (4 * f[A] * f[C]);
-	if (f[D] == 0)
+	f[D] = pow(f[B], 2.0) - (4.0 * f[A] * f[C]);
+	if (f[D] == 0.0)
 	{
-		t = -f[B] / (2 * f[A]);
-		if (t >= 0 && (hit->t == -1 || hit->t > t))
-		set_hit(hit, obj.color, t);
-	}
-	if (f[D] > 0)
-	{
-		t = (-f[B] + f[D]) / (2 * f[A]);
-		if (t >= 0 && (hit->t == -1 || hit->t > t))
+		t = -f[B] / (2.0 * f[A]);
+		if (t >= 0.0 && (hit->t == -1.0 || hit->t > t))
 			set_hit(hit, obj.color, t);
-		t = (-f[B] - f[D]) / (2 * f[A]);
-		if (t >= 0 && (hit->t == -1 || hit->t > t))
+	}
+	else if (f[D] > 0.0)
+	{
+		t = (-f[B] + f[D]) / (2.0 * f[A]);
+		if (t >= 0.0 && (hit->t == -1.0 || hit->t > t))
+			set_hit(hit, obj.color, t);
+		t = (-f[B] - f[D]) / (2.0 * f[A]);
+		if (t >= 0.0 && (hit->t == -1.0 || hit->t > t))
 			set_hit(hit, obj.color, t);
 	}
 	return ;
@@ -39,25 +39,26 @@ void	ray_plane(t_xyz ray, t_plane obj, t_hit *hit, t_xyz camera)
 	float	t;
 	float	d;
 
-	d = innner_product(vec_multiplied(-1, ray), obj.vec);
-	if (d != 0)
+	d = innner_product(vec_multiplied(-1.0, ray), obj.vec);
+	if (d != 0.0)
 	{
 		t = innner_product(vec_subtraction(camera, obj.coord), obj.vec) / d;
-		if (t >= 0 && (hit->t == -1 || hit->t > t))
+		if (t >= 0.0 && (hit->t == -1.0 || hit->t > t))
 			set_hit(hit, obj.color, t);
 	}
 	return ;
 }
 
-// void	ray_cylinder(t_xyz ray, t_object *tmp_obj, t_hit *hit)
+// void	ray_cylinder(t_xyz ray, t_cylinder obj, t_hit *hit, t_xyz camera)
 // {
 // 	float	t;
+// 	float	d;
 
+// 	d = 
 // 	if (t > 0 && hit->t > t)
 // 	{
-// 		hit->t = t;
-// 		hit->colors = tmp_obj->info.cylinder.colors;
-// 		hit->color = tmp_obj->info.cylinder.color;
+// 		if (t >= 0 && (hit->t == -1 || hit->t > t))
+// 			set_hit(hit, obj.color, t);
 // 	}
 // 	return ;
 // }
